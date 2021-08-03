87df6e2822c0e02b6fe197e33fc312693e3e2b4e
Tuan TC <trancongtuan525@gmail.com>
2021-08-03 22:18:52 +0700

    Day-7 build fe router

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -1,3 +1,38 @@
 module.exports = {
   reactStrictMode: true,
 }
+const withPlugins = require("next-compose-plugins");
+const withImages = require("next-images");
+const withSass = require("@zeit/next-sass");
+const withCSS = require("@zeit/next-css");
+const webpack = require("webpack");
+const path = require("path");
+const { i18n } = require("./next-i18next.config");
+
+module.exports = withPlugins([[withSass], [withImages], [withCSS]], {
+  async redirects() {
+    return [
+      {
+        source: '/old-blog/:slug',
+        destination: '/news/:slug', // Matched parameters can be used in the destination
+        permanent: true,
+      },
+    ]
+  },
+  webpack5: false,
+  webpack(config, options) {
+    if (!options.isServer) {
+      config.node = {
+        fs: "empty",
+      };
+    }
+    config.resolve.modules.push(path.resolve("./"));
+    return config;
+  },
+  env: {
+    BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    SECRET_TOKEN_KEY: "token",
+  },
+  // esModule: true,
+  i18n,
+});
--- front-end/pages/_app.js
+++ front-end/pages/_app.js
@@ -1,6 +1,5 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
-import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
