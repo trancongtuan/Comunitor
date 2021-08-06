f71db1fba450cc7d5a81cbf9a0eaf634996083d9
tuan.tc <tuan.tc@neo-lab.vn>
2021-08-06 22:36:32 +0700

    fix storybook

--- front-end/.storybook/main.js
+++ front-end/.storybook/main.js
@@ -1,15 +1,7 @@
-const path = require("path");
-const toPath = (_path) => path.join(process.cwd(), _path);
-
 module.exports = {
-    "stories": [
-        "../stories/**/*.stories.mdx",
-        "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-        "../stories/**/*.stories.tsx"
-    ],
-    "addons": [
-        "@storybook/addon-links",
-        "@storybook/addon-essentials",
-        '@storybook/addon-controls'
-    ],
-}
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  core: {
+    builder: "webpack5",
+  },
+};
