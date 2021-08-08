7269cf956d99d18f34de269a6f4b19bc649d343d
tuan.tc <tuan.tc@neo-lab.vn>
2021-08-08 21:15:49 +0700

    push dashboards

--- front-end/pages/dashboard/index.tsx
+++ front-end/pages/dashboard/index.tsx
@@ -1,6 +1,20 @@
 import React from "react";
-const DashBoard: React.FC = () => {
-  return <div>asdasd</div>;
-};
+import Image from 'next/image'
+const DashBoard: React.FC = () =>  {
+  return (
+    <>
+      <h1>My Homepage</h1>
+      <Image
+        src={"/icon-512x512.png"}
+        alt="Picture of the author"
+        width={200}
+        height={200}
+        layout="fixed"
+        quality="85"
+      />
+      <p>Welcome to my homepage!</p>
+    </>
+  )
+}
 
 export default DashBoard;
